7e0a64bfe84b22f9fd0b86fd78d1fc3fb056d737
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2025-12-11 16:44:41 -0300

    correcao

--- modulo8/ex05/scope_that.py
+++ modulo8/ex05/scope_that.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
-import sys
-
 def add_one(num):
-    print(num + 1)
+    num = num + 1
 
-variavel = sys.argv[1]
+variavel = 0
 print(variavel)
-add_one(int(sys.argv[1]))
+add_one(variavel)
 print(variavel)
